7b5046d6e9f3563d9c4ddd90eb8e50addb540b90
tsw <2235854410@qq.com>
2024-11-09 20:23:33 +0800

    fix val oom

--- train_VIGOR.py
+++ train_VIGOR.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 # os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 # os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 # os.environ["MKL_NUM_THREADS"] = "4" 
@@ -50,6 +52,7 @@ label = area + '_HFoV' + str(FoV)
 ori_noise = args['ori_noise']
 ori_noise = 18 * (ori_noise // 18) # round the closest multiple of 18 degrees within prior 
 print(args)
+print(f"\033[{31}m{time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))}\033[0m")
 wandb.init(project="CCVPE-sampling", name=args['name'], config=args)
 
 
@@ -141,7 +144,7 @@ if training:
 
             # forward + backward + optimize
             logits_flattened, heatmap, ori, matching_score_stacked, matching_score_stacked2, matching_score_stacked3, \
-                    matching_score_stacked4, matching_score_stacked5, matching_score_stacked6 = CVM_model(grd, sat)        
+                    matching_score_stacked4, matching_score_stacked5, matching_score_stacked6 = CVM_model(grd, sat)
 
             loss_ori = orientation_loss(ori, gt_orientation, gt)
             loss_infoNCE = infoNCELoss(torch.flatten(matching_score_stacked, start_dim=1), torch.flatten(gt_bottleneck, start_dim=1))
@@ -158,7 +161,7 @@ if training:
                           , "contrastive_loss4": loss_infoNCE4, "contrastive_loss5": loss_infoNCE5,"contrastive_loss6": loss_infoNCE6, "total loss": loss})
 
 
-           
+
             loss.backward()
             optimizer.step()
 
@@ -179,55 +182,58 @@ if training:
         # validation
         distance = []
         orientation_error = []
-        for i, data in enumerate(val_dataloader, 0):
-            grd, sat, gt, gt_with_ori, gt_orientation, city, _ = data
-            grd = grd.to(device)
-            sat = sat.to(device)
-            gt = gt.to(device)
-            gt_with_ori = gt_with_ori.to(device)
-            gt_orientation = gt_orientation.to(device)
+        torch.cuda.empty_cache()
+        with torch.no_grad():
+            for i, data in enumerate(val_dataloader, 0):
+                grd, sat, gt, gt_with_ori, gt_orientation, city, _ = data
+                grd = grd.to(device)
+                sat = sat.to(device)
+                gt = gt.to(device)
+                gt_with_ori = gt_with_ori.to(device)
+                gt_orientation = gt_orientation.to(device)
 
-            grd_width = int(grd.size()[3] * FoV / 360)
-            grd_FoV = grd[:, :, :, :grd_width]
-            logits_flattened, heatmap, ori, matching_score_stacked, matching_score_stacked2, matching_score_stacked3, \
-                    matching_score_stacked4, matching_score_stacked5, matching_score_stacked6= CVM_model(grd, sat)  
+                grd_width = int(grd.size()[3] * FoV / 360)
+                grd_FoV = grd[:, :, :, :grd_width]
+                logits_flattened, heatmap, ori, matching_score_stacked, matching_score_stacked2, matching_score_stacked3, \
+                        matching_score_stacked4, matching_score_stacked5, matching_score_stacked6= CVM_model(grd, sat)
 
-            gt = gt.cpu().detach().numpy() 
-            gt_with_ori = gt_with_ori.cpu().detach().numpy() 
-            gt_orientation = gt_orientation.cpu().detach().numpy() 
-            heatmap = heatmap.cpu().detach().numpy()
-            ori = ori.cpu().detach().numpy()
-            for batch_idx in range(gt.shape[0]):
-                current_gt = gt[batch_idx, :, :, :]
-                loc_gt = np.unravel_index(current_gt.argmax(), current_gt.shape)
-                current_pred = heatmap[batch_idx, :, :, :]
-                loc_pred = np.unravel_index(current_pred.argmax(), current_pred.shape)
-                pixel_distance = np.sqrt((loc_gt[1]-loc_pred[1])**2+(loc_gt[2]-loc_pred[2])**2)
-                if city[batch_idx] == 'NewYork':
-                    meter_distance = pixel_distance * 0.113248 / 512 * 640
-                elif city[batch_idx] == 'Seattle':
-                     meter_distance = pixel_distance * 0.100817 / 512 * 640
-                elif city[batch_idx] == 'SanFrancisco':
-                    meter_distance = pixel_distance * 0.118141 / 512 * 640
-                elif city[batch_idx] == 'Chicago':
-                    meter_distance = pixel_distance * 0.111262 / 512 * 640
-                distance.append(meter_distance) 
+                gt = gt.cpu().detach().numpy()
+                gt_with_ori = gt_with_ori.cpu().detach().numpy()
+                gt_orientation = gt_orientation.cpu().detach().numpy()
+                heatmap = heatmap.cpu().detach().numpy()
+                ori = ori.cpu().detach().numpy()
+                for batch_idx in range(gt.shape[0]):
+                    current_gt = gt[batch_idx, :, :, :]
+                    loc_gt = np.unravel_index(current_gt.argmax(), current_gt.shape)
+                    current_pred = heatmap[batch_idx, :, :, :]
+                    loc_pred = np.unravel_index(current_pred.argmax(), current_pred.shape)
+                    pixel_distance = np.sqrt((loc_gt[1]-loc_pred[1])**2+(loc_gt[2]-loc_pred[2])**2)
+                    if city[batch_idx] == 'NewYork':
+                        meter_distance = pixel_distance * 0.113248 / 512 * 640
+                    elif city[batch_idx] == 'Seattle':
+                         meter_distance = pixel_distance * 0.100817 / 512 * 640
+                    elif city[batch_idx] == 'SanFrancisco':
+                        meter_distance = pixel_distance * 0.118141 / 512 * 640
+                    elif city[batch_idx] == 'Chicago':
+                        meter_distance = pixel_distance * 0.111262 / 512 * 640
+                    distance.append(meter_distance)
 
-                cos_pred, sin_pred = ori[batch_idx, :, loc_pred[1], loc_pred[2]]
-                if np.abs(cos_pred) <= 1 and np.abs(sin_pred) <=1:
-                    a_acos_pred = math.acos(cos_pred)
-                    if sin_pred < 0:
-                        angle_pred = math.degrees(-a_acos_pred) % 360
-                    else: 
-                        angle_pred = math.degrees(a_acos_pred)
+                    cos_pred, sin_pred = ori[batch_idx, :, loc_pred[1], loc_pred[2]]
+                    if np.abs(cos_pred) <= 1 and np.abs(sin_pred) <=1:
+                        a_acos_pred = math.acos(cos_pred)
+                        if sin_pred < 0:
+                            angle_pred = math.degrees(-a_acos_pred) % 360
+                        else:
+                            angle_pred = math.degrees(a_acos_pred)
 
-                    cos_gt, sin_gt = gt_orientation[batch_idx, :, loc_gt[1], loc_gt[2]]
-                    a_acos_gt = math.acos(cos_gt)
-                    if sin_gt < 0:
-                        angle_gt = math.degrees(-a_acos_gt) % 360
-                    else: 
-                        angle_gt = math.degrees(a_acos_gt)
-                    orientation_error.append(np.min([np.abs(angle_gt-angle_pred), 360-np.abs(angle_gt-angle_pred)]))      
+                        cos_gt, sin_gt = gt_orientation[batch_idx, :, loc_gt[1], loc_gt[2]]
+                        a_acos_gt = math.acos(cos_gt)
+                        if sin_gt < 0:
+                            angle_gt = math.degrees(-a_acos_gt) % 360
+                        else:
+                            angle_gt = math.degrees(a_acos_gt)
+                        orientation_error.append(np.min([np.abs(angle_gt-angle_pred), 360-np.abs(angle_gt-angle_pred)]))
+                torch.cuda.empty_cache()
 
         mean_distance_error = np.mean(distance)
         print('epoch: ', epoch, 'FoV'+str(FoV)+ '_mean distance error on validation set: ', mean_distance_error)
@@ -255,6 +261,7 @@ if training:
 
 
     print('Finished Training')
+    wandb.finish()
 
 else:
     torch.cuda.empty_cache()
